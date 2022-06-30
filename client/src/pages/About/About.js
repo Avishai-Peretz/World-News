bd824cf98837476fd025905411d7b46c4edb3f54
Avishai <Avishai651@gmail.com>
2022-06-30 14:28:24 +0300

    last fix

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -98,8 +98,8 @@ const MainPage = () => {
     intro: {
       en: [
         "200 years ago, the biggest news of the day was, the visit of some new person to the village, Or new royal order.  A trip between counties could take months… years.",
-        "However, these days,  the world is a “small spot” in an elephant's packet, where you can Jump in by a single click ….touch … call, And cross continents in a number of hours.  So, it is good ..  , necessary!To be up to date, and get  the ‘hottest news cakes” of our “small spot ”",
-        "The Israeli community is very diverse and speaks many languages We discovered some need for community and we delivered this application",
+        "However, these days,  the world is a “small spot” in an elephant's packet, where you can Jump in by a single click ….touch … call, And cross continents in a number of hours.  So, it is good ..  , necessary!To be up to date, and get  the ‘hottest news cakes” of our “small spot, ",
+        "The Israeli community is very diverse and speaks many languages We discovered some need for community and we delivered this application.",
         "The application display for users, a number of the latest news  from the world's news Websites from different countries and languages The application allows to users read the news in different languages And all ways to be up to date, and knows what app around the world",
         "Don't forget to take a look, before you take your next fly ",
       ],
