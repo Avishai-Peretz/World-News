c480d217b54a1793a2e095499628503bc15adb5c
Avishai-Peretz <Avishai651@gmail.com>
2022-07-04 01:09:46 +0300

    scheduel func

---
+++ app/controllers/scheduler.js
@@ -0,0 +1,42 @@
+import { Site } from "../models/site/site.model.js";
+import { getYnetData } from "../ynet.js";
+import { getAlbayanData } from "../albayan.js";
+import { getPanetData } from "../panet.js";
+import { getMoscowTimesData } from "../moscowtimes.js";
+import { getNDTVData } from "../jansatta.js";
+import { getDwData } from "../dw.js";
+
+const getSitesData = async (req, res) => {
+  try {
+    let data = await Site.find();
+    if (
+      data.length === 0 || (data.length > 0 && ((new Date).getTime() - (new Date(data[0].createdAt)).getTime())/3600000 > 3)
+    ) {
+      await Site.deleteMany();
+      await getAlbayanData();
+      await getDwData();
+      await getYnetData();
+      await getPanetData();
+      await getMoscowTimesData();
+      await getNDTVData();
+      data = await Site.find();
+    }
+    return res.status(200).send(data);
+  } catch (e) {
+    return res.status(400).send(e.message.toString());
+  }
+};
+
+const getSpecificSiteData = async (req, res) => {
+  try {
+    const site = await Site.findOne({ name: req.params.name });
+    if (!site) {
+      throw Error("Site not found");
+    }
+    return res.status(200).send(site);
+  } catch (e) {
+    return res.status(400).send(e.message.toString());
+  }
+};
+
+getSitesData();
