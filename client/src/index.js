96ecf7fe7816cd208030060849bb59aa90801489
Avishai-Peretz <Avishai651@gmail.com>
2022-07-03 11:33:40 +0300

    Styling

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+debugger
+window.onbeforeunload = function(event)
+{
+  localStorage.removeItem("sixTopArticles");
+};
+
 root.render(
   <React.StrictMode>
       <App />
--- client/src/pages/home/Homepage.jsx
+++ client/src/pages/home/Homepage.jsx
@@ -3,9 +3,10 @@ import axios from "axios";
 import Article from "./articles/Articles.jsx";
 import { Link } from "react-router-dom";
 import { myContext } from "../../context/language.js";
-import "./homepage.css";
 import { Splide, SplideSlide} from '@splidejs/react-splide';
-import '@splidejs/react-splide/css';
+// import '@splidejs/react-splide/css';
+import '@splidejs/splide/css/sea-green';
+import "./homepage.css";
 
 const Homepage = () => {
   
@@ -28,14 +29,52 @@ const Homepage = () => {
   return (
     <div className="page">
       <div className="articles-container">
+    <div>          
         <Splide
-        options={ {
+          options={{
           rewind: true,
-          width: "95vw",
-          gap   : '1rem',
-          type   : 'loop',
+          width: "100vw",
+          height: "70vh",
+          gap   : '3rem',
+          type: 'loop',
+          padding: '0rem',
           perPage: 3,
-          focus  : 'center',
+          // perMove: 3,
+          focus  : 'start',
+          arrow: "splide__arrows arrows-c",
+          wheel: true,
+        } }
+        >
+        {getSixTopArticles
+          ? getSixTopArticles.map((article) => {
+              const topArticle = article;
+              return (
+                  <SplideSlide>
+                  <Link key={"link" + topArticle._id} to={`/article/${article._id}`} className="article-link" style={{ textDecoration: 'none' }}>
+                    <Article
+                      key={topArticle._id}
+                      lang={lang}
+                      article={topArticle}
+                      />
+                  </Link>
+                    </SplideSlide>
+              );
+            })
+            : <div className="loader-container"><div className="loader"></div></div>}
+        </Splide>
+        </div>
+        {/* <div>
+        <Splide
+          options={{
+          rewind: true,
+            width: "100vw",
+          height: "10vh",
+          gap   : '3rem',
+          type: 'loop',
+          padding: '5rem',
+          perPage: 3,
+          // perMove: 3,
+          focus  : 'start',
           arrow: "splide__arrows arrows-c",
           wheel: true,
         } }
@@ -57,6 +96,7 @@ const Homepage = () => {
             })
             : <div className="loader-container"><div className="loader"></div></div>}
             </Splide>
+        </div> */}
       </div>
       <div className="articles-container-mobile">
         {getSixTopArticles
