0c7c1110a741d3f171c39810df3edaf9a050520d
Avishai-Peretz <Avishai651@gmail.com>
2022-07-04 01:28:43 +0300

    api call and scheduel func

--- app/controllers/scheduler.js
+++ app/controllers/scheduler.js
@@ -9,34 +9,19 @@ import { getDwData } from "../dw.js";
 const getSitesData = async (req, res) => {
   try {
     let data = await Site.find();
-    if (
-        data.length === 0 || data.length < 5 || (data.length > 0 && ((new Date).getTime() - (new Date(data[0].createdAt)).getTime())/3600000 > 3)
-    ) {
-      await Site.deleteMany();
-      await getAlbayanData();
-      await getDwData();
-      await getYnetData();
-      await getPanetData();
-      await getMoscowTimesData();
-      await getNDTVData();
-      data = await Site.find();
-    }
+    await Site.deleteMany();
+    await getAlbayanData();
+    await getDwData();
+    await getYnetData();
+    await getPanetData();
+    await getMoscowTimesData();
+    await getNDTVData();
+    data = await Site.find();
     return res.status(200).send(data);
   } catch (e) {
     return res.status(400).send(e.message.toString());
   }
 };
 
-const getSpecificSiteData = async (req, res) => {
-  try {
-    const site = await Site.findOne({ name: req.params.name });
-    if (!site) {
-      throw Error("Site not found");
-    }
-    return res.status(200).send(site);
-  } catch (e) {
-    return res.status(400).send(e.message.toString());
-  }
-};
 
 getSitesData();
