79297d98c8f0ca82ea3fa325e03a124aedbf91e7
Avishai-Peretz <Avishai651@gmail.com>
2022-07-04 03:15:39 +0300

    scheduel func v3 api fix

--- app/controllers/scheduler.js
+++ app/controllers/scheduler.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 const URI = (() => {
     if (process.env.NODE_ENV === "production") {
-      return "/api";
+      return "https://worldwide-news-hackathon.herokuapp.com/api";
     } else {
       return "http://localhost:5050/api";
     }
 })();
   
 const getScheduledSitesData = async () => {
-    const articles = await axios.get("/api");
+    const articles = await axios.get(`${URI}`);
 };
 
 getScheduledSitesData();
