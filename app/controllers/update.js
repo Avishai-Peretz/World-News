325117246e07198269f0fb07000731d9ee3e84dd
Avishai-Peretz <Avishai651@gmail.com>
2022-07-05 23:31:26 +0300

    updating file created

---
+++ app/controllers/update.js
@@ -0,0 +1,38 @@
+import { getArticles } from "./site.controllers.js";
+import { Site } from "./models/site/site.model.js";
+import { getYnetData } from "../ynet.js";
+import { getAlbayanData } from "../albayan.js";
+import { getPanetData } from "../panet.js";
+import { getMoscowTimesData } from "../moscowtimes.js";
+import { getNDTVData } from "../jansatta.js";
+import { getDwData } from "../dw.js";
+
+
+(async (req, res) => {
+  try {
+    let data = await Site.find();    
+    const getTheArticles =await getArticles();
+    const articles = getTheArticles ? getTheArticles : [];
+    if (
+      (articles.length < 6 || data.length < 6 ) || ((data.length > 0 || articles.length >0) && ((new Date).getTime() - (new Date(data[0].createdAt)).getTime())/3600000 > 1)
+    ) {
+      await getAlbayanData();
+      console.log("1")
+      await getPanetData();
+      console.log("2")
+      await getDwData();
+      console.log("3")
+      await getYnetData();
+      console.log("4")
+      await getMoscowTimesData();
+      console.log("5")
+      await getNDTVData();
+      console.log("6")
+      data = await Site.find();
+    }
+  } catch (e) {
+    throw new Error(e);
+  }
+  console.log("success");
+})()
+console.log("run")
