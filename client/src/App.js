8ba5699a1949c590fd1fbc942f90a935ec254d67
Avishai-Peretz <Avishai651@gmail.com>
2022-07-06 08:16:54 +0300

    local storage update fix

--- client/src/App.js
+++ client/src/App.js
@@ -12,21 +12,11 @@ import "./App.css";
 
 function App() {
   
-  const { setTopArticles, URI, sixTopArticles, lang, } = useContext(myContext);
+  const { setTopArticles, URI, sixTopArticles, lang, refreshLocal, saveToContext } = useContext(myContext);
   
   
   const topArticles = async () => {
-    const refreshLocal = async () => {
-      localStorage.removeItem('localArticles')
-      const articles = await axios.get(`${URI}`);
-      const localArticles = JSON.stringify([{ localUpdateTime: new Date() }, ...articles.data]);
-      localStorage.setItem("localArticles", localArticles);
-    }
-    const saveToContext = () => {
-      const getLocalArticles = JSON.parse(localStorage.getItem('localArticles'));
-      const articlesList = getLocalArticles.slice(1)
-      return articlesList
-    };
+
     const getLocalArticles = () => {
       const local = JSON.parse(localStorage.getItem('localArticles'));
       if (local && local.length < 7) {
@@ -38,7 +28,7 @@ function App() {
       else return false;
     };
     if ( getLocalArticles() ) {
-      refreshLocal()
+      await refreshLocal()
     }
     setTopArticles(saveToContext());
   };
