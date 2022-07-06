d9c16de3d726d861036da0ad98b607df34dfb1e2
Avishai-Peretz <Avishai651@gmail.com>
2022-07-06 07:42:49 +0300

    scraping fix

--- app/Utils/startUpdate.js
+++ app/Utils/startUpdate.js
@@ -1,11 +1,18 @@
 import getScheduledSitesData from "../controllers/scheduler.js";
 
 const startUpdate = async () => {
-    try {
-        getScheduledSitesData()
+  const URI = (() => {
+    if (process.env.NODE_ENV === "production") {
+      return "https://worldwide-news-hackathon.herokuapp.com/update";
+    } else {
+      return "http://localhost:5050/update";
     }
-    catch {
-      return [];
-    }
+  })();
+  try {
+    const article = await axios.get(URI);
+    return res.status(200).send(article.data);
+  } catch (e) {
+    return res.status(400).send(e.message.toString());
+  }
 }
 startUpdate();
